6a7114d9a433daa9f1964916f86ada28c43a4933
gerrit developer <dev@example.com>
2018-05-18 00:28:13 +0200

    Añadir información commit en REST interface
    Change-Id: I84f1e24faf3db5bd59c9a3c3ea2305d6aad2daac

---
+++ src/main/java/es/codeurjc/ais/tictactoe/VersionController.java
@@ -0,0 +1,42 @@
+package es.codeurjc.ais.tictactoe;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class VersionController {
+
+    @RequestMapping(value = "/version", method = GET)
+    public String versionInformation() {
+        return readGitProperties();
+    }
+
+    private String readGitProperties() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+        try {
+            return readFromInputStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Version information could not be retrieved";
+        }
+    }
+
+    private String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+}
